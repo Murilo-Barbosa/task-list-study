ba2f2728e8059d047fcdd7dd9efdd2e571aa18b3
Murilo Barbosa <murilobarbosadarosa2002@gmail.com>
2022-08-18 20:07:05 -0300

    Commit Unico
    Projeto completo

---
+++ scr/main.js
@@ -0,0 +1,79 @@
+const inputElement = document.querySelector(".new-task-input");
+const addTaskButton = document.querySelector(".new-task-button");
+const tasksContainer = document.querySelector(".tasks-container");
+
+const validateInput = () => {
+  return inputElement.value.trim().length > 0;
+};
+
+const handleAddTask = () => {
+  const inputIsValid = validateInput();
+
+  if (!inputIsValid) {
+    return inputElement.classList.add("error");
+  }
+
+  const taskItemDiv = document.createElement("div");
+  taskItemDiv.classList.add("task-item");
+
+  const taskContent = document.createElement("p");
+  taskContent.innerText = inputElement.value;
+
+  taskContent.addEventListener("click", () => handleClick(taskContent));
+
+  const deleteItem = document.createElement("i");
+  deleteItem.classList.add("far");
+  deleteItem.classList.add("fa-trash-alt");
+
+  deleteItem.addEventListener("click", () =>
+    handleDeleteClick(taskItemDiv, taskContent)
+  );
+
+  taskItemDiv.appendChild(taskContent);
+  taskItemDiv.appendChild(deleteItem);
+
+  tasksContainer.appendChild(taskItemDiv);
+
+  inputElement.value = "";
+
+  updateLocalStorage();
+};
+
+const handleClick = (taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
+
+    if (currentTaskIsBeingClicked) {
+      task.firstChild.classList.toggle("completed");
+    }
+  }
+
+  updateLocalStorage();
+};
+
+const handleDeleteClick = (taskItemDiv, taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent);
+
+    if (currentTaskIsBeingClicked) {
+      taskItemDiv.remove();
+    }
+  }
+
+  updateLocalStorage();
+};
+
+const handleInputChange = () => {
+  const inputIsValid = validateInput();
+
+  if (inputIsValid) {
+    return inputElement.classList.remove("error");
+  }
+};
+
+addTaskButton.addEventListener("click", () => handleAddTask());
+inputElement.addEventListener("change", () => handleInputChange());
